36be402e4ef60a40b298ea5c4a53a1daec5c442a
Julia Golubkova <golubkovjul@gmail.com>
2024-11-18 12:22:09 +0300

    delete PhotoRepository

--- src/main/java/com/example/backend/Repositories/PhotoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.backend.Repositories;
-
-import com.example.backend.Entity.Category;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PhotoRepository extends CrudRepository<Category, Integer> {
-}
